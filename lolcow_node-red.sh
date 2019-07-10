561f650510803b094996f9ca7d10c5da6a720109
Sami Karkinen <sami.karkinen@kavokerr.com>
2019-07-10 16:26:52 +0300

    lolcow_node-red.sh: Commented in the script file the usage and likely modifications for different systems

--- lolcow_node-red.sh
+++ lolcow_node-red.sh
@@ -1,3 +1,8 @@
+# You might need full path instead of just program names depending on your setup
+
 tmux new-session -d 'node-red | lolcat'
 tmux split-window -dvl 12 'cowsay -f tux "This works only in a default sized terminal window. If the terminal window is full screen or resized in any way, the split size gets f'\''d up." | lolcat; cowsay'
+
+# Uncomment the following and remove the other split-window d parameter for system monitoring at the same time
+#tmux split-window -h 'top'
 tmux attach-session
